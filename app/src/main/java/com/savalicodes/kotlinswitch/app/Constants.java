f6debeccc96b482df7ddf3657b85db979a2949a9
fesavali <felixnzioki99@gmail.com>
2021-07-30 15:24:34 +0300

    dad

--- app/src/main/java/com/savalicodes/kotlinswitch/app/Constants.java
+++ app/src/main/java/com/savalicodes/kotlinswitch/app/Constants.java
@@ -7,9 +7,9 @@ public class Constants {
 	public static final int SEARCH_BY_USER = 2;
 
 	public static final String APP_SHARED_PREFERENCES = "app_sp";
-	public static final String KEY_PERSON_NAME = "person_name";
+	public static final String KEY_PERSON_NAME = "persons_name";
 
-	public static final String KEY_GITHUB_USER = "user";
+	public static final String KEY_GITHUB_USER = "users";
 	public static final String KEY_QUERY_TYPE = "query_type";
 	public static final String KEY_LANGUAGE = "language";
 	public static final String KEY_REPO_SEARCH = "query_repo";
